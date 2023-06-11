db8c3734718b68e0ec6ee011ea2ae8345b551b50
thomas brockmans <thomasbrockmans@icloud.com>
2023-06-11 22:00:33 +0200

    header navigation created

--- scripts/code.js
+++ scripts/code.js
@@ -1,1 +1,28 @@
 console.log("the code script is loaded");
+
+const setup =()=> {
+  let dropdownicon = document.getElementById("headerdropdownicon");
+  dropdownicon.addEventListener("click", dropdownmenu);
+}
+
+window.addEventListener("load", setup);
+
+const dropdownmenu =()=> {
+  let navmenu = document.getElementsByClassName('nav');
+  for(let i = 0 ; i < navmenu.length ; i++) {
+    navmenu[i].style.display = "inline";
+  }
+
+  console.log("dropdownmenu should be shown");
+
+  setTimeout(dropdownmenuhide, 10000);
+};
+
+const dropdownmenuhide =()=> {
+  let navmenu = document.getElementsByClassName('nav');
+  for(let i = 0 ; i < navmenu.length ; i++) {
+    navmenu[i].style.display = "none";
+  }
+
+  console.log("dropdownmenu should be hidden");
+}
