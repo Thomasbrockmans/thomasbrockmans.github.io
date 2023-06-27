077c70094480e57c7c709fabc114b4b0b86d3a9a
thomas brockmans <thomasbrockmans@icloud.com>
2023-06-27 20:39:24 +0200

    fixed menu click

--- scripts/code.js
+++ scripts/code.js
@@ -1,11 +1,26 @@
+let global = {
+  menuVisible: false
+};
+
 console.log("the code script is loaded");
+console.log("the menu is visible: " + global.menuVisible);
 
 const setup =()=> {
   let dropdownicon = document.getElementById("headerdropdownicon");
-  dropdownicon.addEventListener("click", dropdownmenu);
+  dropdownicon.addEventListener("click", clickmenu);
 }
 
 window.addEventListener("load", setup);
+
+
+const clickmenu =()=> {
+  if(global.menuVisible == false){
+    dropdownmenu();
+  }
+  else {
+    dropdownmenuhide();
+  }
+}
 
 const dropdownmenu =()=> {
   let navmenu = document.getElementsByClassName('nav');
@@ -13,6 +28,7 @@ const dropdownmenu =()=> {
     navmenu[i].style.display = "inline";
   }
 
+  global.menuVisible = true;
   console.log("dropdownmenu should be shown");
 
   setTimeout(dropdownmenuhide, 10000);
@@ -23,6 +39,6 @@ const dropdownmenuhide =()=> {
   for(let i = 0 ; i < navmenu.length ; i++) {
     navmenu[i].style.display = "none";
   }
-
+  global.menuVisible = false;
   console.log("dropdownmenu should be hidden");
 }
