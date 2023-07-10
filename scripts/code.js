978d0c276a77d9a982a00647fc78ce8cecbc1781
thomas brockmans <thomasbrockmans@icloud.com>
2023-07-10 21:15:54 +0200

    homepage done

--- scripts/code.js
+++ scripts/code.js
@@ -1,45 +1,10 @@
-let global = {
-  menuVisible: false
-};
-
-console.log("the code script is loaded");
-console.log("the menu is visible: " + global.menuVisible);
-
 const setup =()=> {
-  let dropdownicon = document.getElementById("headerdropdownicon");
-  dropdownicon.addEventListener("click", clickmenu);
-
+  let stylebutton = document.getElementById("style");
+  stylebutton.addEventListener('Click', style);
 }
 
-window.addEventListener("load", setup);
+window.addEventListener('load', setup);
 
-
-const clickmenu =()=> {
-  if(global.menuVisible == false){
-    dropdownmenu();
-  }
-  else {
-    dropdownmenuhide();
-  }
-}
-
-const dropdownmenu =()=> {
-  let navmenu = document.getElementsByClassName('nav');
-  for(let i = 0 ; i < navmenu.length ; i++) {
-    navmenu[i].style.display = "inline";
-  }
-
-  global.menuVisible = true;
-  console.log("dropdownmenu should be shown");
-
-  setTimeout(dropdownmenuhide, 10000);
+const style =()=> {
+  window.alert("other style");
 };
-
-const dropdownmenuhide =()=> {
-  let navmenu = document.getElementsByClassName('nav');
-  for(let i = 0 ; i < navmenu.length ; i++) {
-    navmenu[i].style.display = "none";
-  }
-  global.menuVisible = false;
-  console.log("dropdownmenu should be hidden");
-}
